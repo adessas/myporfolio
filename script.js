7b73686c3990457794b88cd81d34fc7eba1c43fa
adessas <falsis.adessa.isabel.badal@gmail.com>
2024-12-10 17:57:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+});
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+window.onclick = function (event) {
+    var modals = document.getElementsByClassName('modal');
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = 'none';
+        }
+    }
+}
